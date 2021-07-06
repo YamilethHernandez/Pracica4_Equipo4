64d5a5749029fded42606a6d747a6e9de1637bc6
gerardo-app <84480810+gerardo-app@users.noreply.github.com>
2021-07-06 17:13:51 -0500

    Update muestraJSON.py
    Se agregarón dos decoradores, uno para el método de mofidicar la lista de integrantes y el otro método para actualizar los datos modificados.

--- muestraJSON.py
+++ muestraJSON.py
@@ -78,3 +78,37 @@ async def eliminar(request:Request,id:int):
 
     return RedirectResponse("/lista",303)
 
+@app.get("/modificar/{id}")
+async def actualizar(request:Request, id:int):
+    datos = await cargarJSON()
+    alumno = datos[id]
+    return miPlantilla.TemplateResponse("listaIntegrantes.html",{"request":request,"lista":datos, "alumno": alumno})
+
+@app.post("/actualizar")
+async def actualizar(request:Request):
+    datos = await cargarJSON()
+    nuevos_datos = {}
+    datos_formulario = await request.form()
+    #print(len(datos))
+    #print(datos)
+    #print("fin de prueba")
+    id_editar = int(datos_formulario["item_index"])  #valor del ide del ultimo elemento de la lista
+    #print(datos_formulario)
+    #print(datos_formulario["f_nombre"])
+    #print(datos_formulario.items)
+    nuevos_datos["item_id"] = int(datos_formulario["item_id"])
+    nuevos_datos["matricula"] = int(datos_formulario["f_matricula"])
+    nuevos_datos["nombre"] = datos_formulario["f_nombre"]
+    nuevos_datos["edad"] = int(datos_formulario["f_edad"])
+    nuevos_datos["APaterno"] = datos_formulario["f_APaterno"]
+    nuevos_datos["AMaterno"] = datos_formulario["f_AMaterno"]
+    nuevos_datos["correo"] = datos_formulario["f_correo"]
+    nuevos_datos["telefono"] = int(datos_formulario["f_telefono"])
+    nuevos_datos["carrera"] = datos_formulario["f_carrera"]
+    #print(nuevos_datos)
+    datos[id_editar] = nuevos_datos
+    #print(datos)
+
+    await guardarJSON(datos)
+
+    return RedirectResponse("/lista",303)
